a5678d1d4df04530b53a42aaa57f5ce28ff334c3
rahul wasatkar <rswasatkar25@gmail.com>
2023-02-21 20:55:04 +0530

    1st commit

--- BranchService/src/main/java/com/jbk/branchserviceApplication2.java
+++ BranchService/src/main/java/com/jbk/branchserviceApplication2.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class branchserviceApplication {
+public class branchserviceApplication2 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(branchserviceApplication.class, args);
-		System.out.println("hiii ");
+		SpringApplication.run(branchserviceApplication2.class, args);
 	}
 
 }
